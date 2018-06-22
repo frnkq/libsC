6ce6cc669ba6d1f9f31bf5b52c6c26b76611cdad
Franco Canevali <mail@francocanevali.com>
2018-06-21 22:16:31 -0300

    Modified ABM.h

--- ABM.h
+++ ABM.h
@@ -2,8 +2,8 @@
 #define ABM_H_INCLUDED
 
 #include "ArrayList.h"
-int alta(ArrayList* arrayList);
-int modificacionByIndex(ArrayList* arrayList, int i);
-int baja(ArrayList* arrayList);
+void alta(ArrayList* arrayList);
+void modificacionByIndex(ArrayList* arrayList, int i);
+void baja(ArrayList* arrayList);
 
 #endif // ABM_H_INCLUDED
